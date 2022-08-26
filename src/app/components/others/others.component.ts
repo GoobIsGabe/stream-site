ed070c9a2fa1a1a43ebe4b8df972f2911d51c70d
GoobIsGabe <gabe.harp1996@gmail.com>
2022-08-25 22:22:28 -0400

    https://www.delftstack.com/howto/angular/foreach-loop-in-angular/  Please enter the commit message for your changes. Lines starting Additional changes to community page

--- src/app/components/others/others.component.ts
+++ src/app/components/others/others.component.ts
@@ -10,6 +10,9 @@ export class OthersComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const randomNum = "#"+((1<<24)*Math.random()|0).toString(16); 
+
+  document.documentElement.style.setProperty('--main-bg-color', randomNum);
   }
 
 }
