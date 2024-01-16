f79bdd0ee813e502c2908d185127bfc16e6e0c3d
GoobIsGabe <gabe.harp1996@gmail.com>
2024-01-16 14:02:49 -0500

    Committing changes before Angular CLI update

--- src/app/components/socials/socials.component.ts
+++ src/app/components/socials/socials.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+// import { YoutubeService } from 'src/app/services/youtube.service';
 
 @Component({
   selector: 'app-socials',
   templateUrl: './socials.component.html',
-  styleUrls: ['./socials.component.scss']
+  styleUrls: ['./socials.component.scss'],
 })
 export class SocialsComponent implements OnInit {
-
-  constructor() { }
+  videos: any[];
+  // constructor(private youTubeService: YoutubeService) {}
 
   ngOnInit(): void {
+    // this.videos = [];
+    // this.youTubeService
+    //   .getVideosForChanel('UCfureA_XfafdyGch4IaEllg', 1)
+    //   .pipe()
+    //   .subscribe((lista) => {
+    //     for (let element of lista['items']) {
+    //       this.videos.push(element);
+    //       console.log(element);
+    //     }
+    //   });
   }
-
 }
